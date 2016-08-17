33a88a28c3c78d3b107bce4355c4d7097ed3f286
MichaeGon <lodnix@gmail.com>
2016-08-18 08:14:13 +0900

    css

--- errorhome.elm
+++ errorhome.elm
@@ -1,8 +1,11 @@
+module Error exposing (..)
+
 import Html exposing (..)
 import Html.App as App
 import Html.Attributes exposing (..)
 --import Html.Events exposing (..)
 --import Random exposing (..)
+import String exposing (..)
 
 main : Program Never
 main = App.program
@@ -17,6 +20,7 @@ main = App.program
 type alias Model =
     { ecode : Int
     , etitle : String
+    , emessage : String
     }
 
 
@@ -38,14 +42,45 @@ subscriptions model = Sub.none
 -- init
 
 init : (Model, Cmd Msg)
-init = (Model 200 "OK", Cmd.none)
+init = (Model 200 "OK" "", Cmd.none)
 
 -- view
 
 view : Model -> Html Msg
 view model = div []
-    [ h1 [] [ text model.etitle ]
+    [ h1 [] <| mkTitle model.etitle
     , br [] []
+    , text model.emessage
     , br [] []
     , hr [] []
+    , bodySecond
+    ]
+
+mkTitle : String -> List (Html Msg)
+mkTitle s =
+    case uncons s of
+        Just (x, xs) ->
+            a
+            [ tytleStyle
+            , target "_blank"
+            , href "cgi/jump.cgi?index=3810954172703717506"
+            ]
+            [ text <| fromChar x ] :: mkTitle xs
+        _ ->
+            []
+
+bodySecond : Html Msg
+bodySecond = div [footerStyle]
+    [ text "Apache/"]
+
+tytleStyle : Attribute Msg
+tytleStyle = style
+    [ ("text-decoration", "none")
+    , ("color", "black")
+    ]
+
+footerStyle : Attribute Msg
+footerStyle = style
+    [ ("text-align", "left")
+    , ("font-style", "italic")
     ]
